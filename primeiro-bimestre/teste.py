971175fdb6c6561c3c9a6036fa1bf0317e19f3bf
Lucas Pacífico <112137462+lvcaspacifico@users.noreply.github.com>
2024-03-21 18:40:35 -0300

    versão inicial do trabalho adicionada

--- primeiro-bimestre/teste.py
+++ primeiro-bimestre/teste.py
@@ -1,3 +1,33 @@
 # Testando se vai atualizar no Github
 
-print("Hello world")
+class Processo():
+    def __init__(self, tamanho, posicaoAtual):
+        self.tamanho = tamanho
+        self.posicaoAtual = posicaoAtual
+
+
+class AlgoritmoSTRF():
+    def __init__(self):
+        contadorCPU = []
+        rodar = True
+        contador = 0
+        while (rodar):
+            
+            print("///////// Bem-vindo ao Algoritmo STRF /////////")
+            print("///////// 'shortest time remaining first /////////")
+            print("==================================================")
+
+            print("Adicione um Processo na fila:")
+
+            processoAtual = Processo(int(input("Digite o tamanho do processo(0 a 10):")),contador)
+            contadorCPU.append(processoAtual)
+
+
+            print(contadorCPU)
+            op = int(input("Digite 4 para sair"))
+            if op == 4:
+                rodar = False
+            contador += 1
+
+
+AlgoritmoSTRF()
